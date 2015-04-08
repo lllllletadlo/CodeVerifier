d5b7cae56b700c4c97d5b3f062ba29d8bd5677fe
zdendapa <zdenek.pavlicek@gmail.com>
2015-04-08 08:55:06 +0200

    t2-funkcni

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,52 @@
-validator = null;
+var scanData = [];
 
 function onDeviceReady() {
 
 
-    //scripDefaultInit();
 
-    //hideAll();
-    //showWindow("contentIndex");
+    scripDefaultInit();
 
-    window.plugins.imeiplugin.getImei(getImei_handler);
+    storage_read();
+
+    if(!local)
+    {
+        window.plugins.imeiplugin.getImei(getImei_handler);
+    }
+
+
+    $("#specInfo").css("display","none");
+
+    showWindow("screenScan");
+
+}
 
 
 
 
+function clickInit()
+{
+    $("#memmory").on("click", "h1", function (e) {
+
+        screenDetail_draw(scanData[$(this).attr("data-index")]);
+        showWindow("screenDetail");
+    });
 }
 
 function showWindow(windowName)
 {
 
-    //hideAll();
+    $("#screenNastaveni").css("display","none");
+    $(".footscreenNastaveni span").css("color","#798898");
+    $("#screenScan").css("display","none");
+    $(".footscreenScan span").css("color","#798898");
+    $("#screenDetail").css("display","none");
+    $(".footscreenDetail span").css("color","#798898");
+
+    $("#"+windowName).css("display","block");
+    $(".foot"+windowName + " span").css("color","white");
 
 
-    if(windowName=="contentIndex")
-    {
-        containerVisibilitySet("contentIndex",true);
 
-    }
 
 
 }
@@ -46,7 +67,8 @@ function scanBarcode() {
     {
         result.text = "text";
         result.format = "format";
-        $('#scanResult').val(result.text + "/" + result.format);
+        $('#scanResult').val("IqiW87hcEZg");
+        ajax_send();
     } else
     {
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
@@ -67,4 +89,136 @@ function scanBarcode() {
     }
 
 
+}
+
+
+
+function ajax_send()
+{
+    $.ajax({
+        //url: url + "/tokens/show/QwSwVL5Py5g=.json",
+        url: $("#setServer").val() + "tokens/show/"+$('#scanResult').val()+"=.json",
+        type: 'POST',
+        data: JSON.stringify({
+            Username: "frantisek",
+            Password: "heslo",
+            IMEI: "123456789012345"
+        }),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: ajax_dataProceed,
+        error: ajaxErrorHandler
+    });
+}
+
+function ajax_dataProceed(data)
+{
+    screenDetail_draw(data);
+    showWindow('screenDetail');
+
+    scanData[scanData.length] = data;
+    if(scanData.length>3)
+    {
+        scanData = scanData.slice(scanData.length-3,scanData.length);
+    }
+
+    memmoryRedraw();
+}
+
+
+function memmoryRedraw()
+{
+    $("#memmory").empty();
+
+    for(var i in scanData){
+        console.log(scanData[i].Code);
+        $("#memmory").append('<h1 data-index="'+i+'" class="_buttonClick buttonOpacity buttonMemmory">'+ scanData[i].Code+'</h1>');
+    }
+
+}
+
+
+
+function screenDetail_draw(data)
+{
+    var numRows = 0;
+    console.log(data);
+    if(data!=null)
+    {
+        strData = "";
+        for(var i in data){
+            strData += i + "=" + data[i] + "\n";
+            numRows++;
+        }
+    }
+
+
+    $("#screenDetail textarea").val(strData);
+    $("#screenDetail textarea").css("height",numRows + 4 + "em");
+}
+
+function ajaxErrorHandler(data) {
+    console.log(data);
+
+
+    if(!local)
+    {
+        if(typeof navigator.connection!="undefined")
+        {
+
+            var networkState = navigator.connection.type;
+            if(networkState == Connection.UNKNOWN || networkState== Connection.NONE)
+            {
+                alertG("Nelze se připojit k internetu","Chyba!");
+                return;false
+            }
+        }
+
+    }
+
+    var msg = "";
+    if(typeof data.msg != "undefined")
+    {
+        msg = data.msg;
+    }
+
+    if(typeof data.responseText != "undefined")
+    {
+        msg = data.responseText;
+    }
+
+    if(msg=="")
+    {
+        alertG("Chyba s komunikací se serverem","Chyba!");
+    } else
+    {
+        alertG("chyba:" +data.msg,"Chyba!");
+    }
+}
+
+function storage_write()
+{
+    localStorage.setItem("CodeVerifier_urlServer",$("#setServer").val());
+    localStorage.setItem("CodeVerifier_userName",$("#setUserName").val());
+    localStorage.setItem("CodeVerifier_password",$("#setPassword").val());
+    alertG("Uloženo");
+}
+
+function storage_read()
+{
+    if(typeof(Storage) !== "undefined") {
+        var urlServer = localStorage.getItem("CodeVerifier_urlServer");
+        var userName = localStorage.getItem("CodeVerifier_userName");
+        var password = localStorage.getItem("CodeVerifier_password");
+
+        if(urlServer!=null)
+            $("#setServer").val(urlServer);
+        if(userName!=null)
+            $("#setUserName").val(userName);
+        if(password!=null)
+            $("#setPassword").val(password);
+
+    } else {
+        // Sorry! No Web Storage support..
+    }
 }
--- js/scriptsDefault.js
+++ js/scriptsDefault.js
@@ -1,6 +1,55 @@
+/**
+ * array of screens
+ *
+ * @type {{}}
+ */
 var transitionObject = {};
 
+var support = {
+    supportedTran : 1,
+    supportedTouchStartEven : "touchstart",
+    sccanerBarcode : false
+};
 
+var scriptDef = {
+
+    transition : {
+        transitionInAction : false,
+        transitionTranslateEndFunction : function(){
+            this.transitionInAction = false;
+            console.log(this.transitionInAction);
+        }
+    },
+    /*
+     for these function to support ios7 must be in config:
+     - org.apache.cordova.statusbar
+     - disabled with this:
+
+     <gap:config-file platform="ios" parent="UIStatusBarHidden">
+     <true/>
+     </gap:config-file>
+     <gap:config-file platform="ios" parent="UIViewControllerBasedStatusBarAppearance">
+     <false/>
+     </gap:config-file>
+
+     */
+    iosStatusBarShow : function(){
+        var ios = navigator.userAgent.match(/iphone|ipad|ipod/i) &&
+            parseInt(navigator.appVersion.match(/OS (\d)/)[1], 10) >= 7;
+        if (ios) {
+            document.body.style.webkitTransform = 'translate3d(0, 20px, 0)';
+            $("body").css("height",$(window).height()-20+"px");
+            if(typeof  StatusBar != "undefined")
+            {
+                StatusBar.show();
+            }
+        }
+    }
+};
+
+/**
+ * Initialize application.
+ */
 function scripDefaultInit() {
 
     // search first page display usually in dataManagerLoad after ajax result
@@ -9,145 +58,136 @@ function scripDefaultInit() {
     supportDetectDef();
 
     // by supported aceleration, prepare classes and view
-    transitionInit();
+    transitionInitDef();
 
     // set clicks function on buttons, touch or click
     clickInitDef();
 
     // load variable from localStore and set data it in pages
-    dataManagerLoad();
+    dataManagerLoadDef();
 
     // unhide ready app :)
-    $(".special.cover").css("display","none");
-
+    $(".special.cover").css("display", "none");
 
 }
 
-function clickInitDef()
-{
-
-    var $buttons;
+/**
+ * Initialize buttons to be clickable, adds onClick listener to all buttons which have _buttonClick class
+ */
+function clickInitDef() {
     // ------------- buttons needs click (scrollable or...)
-    $buttons = $('._buttonClick');
-
-    // !!! on iPhone it fires twice with that way:
-    //$buttons.on("click", function(e){
-    $buttons.off('click').on('click', function(e){
+    //$(document).off('click').on('click', function(e){
+    $(document).on("click", "._buttonClick", function (e) {
         e.stopPropagation();
-        if($(this).hasClass("buttonDisable"))
+        if ($(this).hasClass("buttonDisable"))
             return;
-        if($(this).hasClass("buttonOpacity"))
+        if ($(this).hasClass("buttonOpacity"))
             $(this).addClass('highlightOpacity');
         else
             $(this).addClass('highlight');
-        var el= this;
+        var el = this;
 
-        setTimeout(function(){
-            // set button click effect back
+        setTimeout(function () {
             var dataClick = $(el).attr("data-click");
-            if($(el).hasClass("buttonOpacity"))
+            if ($(el).hasClass("buttonOpacity"))
                 $(el).removeClass('highlightOpacity');
             else
                 $(el).removeClass('highlight');
-
-            // is there any validation to proceed?
-            var validateFormID = $(el).attr("data-validace-submitForm");
-            if(validateFormID!= null && validator!=null)
-            {
-
-                var isValid = validator.valideForm(validateFormID);
-                if(!isValid) return;
-            }
-
-            // run onclick function
-            if(dataClick!= null)
-            {
+            if (dataClick != null) {
                 eval(dataClick);
             }
-        },150);
+        }, 150);
     });
 
-    if (typeof clickInit == 'function') { clickInit(); }
-
-}
-
-function dataManagerLoad()
-{
-
-}
-
-function showInfow(show,msg)
-{
-
-    if(show)
-    {
-        $("div.special.info h1").html(msg);
-        //$("div.special.info").css("display","block");
-        containerVisibilitySet("specInfo",true);
-    } else
-    {
-        //$("div.special.info").css("display","none");
-        containerVisibilitySet("specInfo",false);
+    if (typeof clickInit != "undefined") {
+        clickInit();
     }
 
+    if (typeof focusOutInit != "undefined") {
+        focusOutInit();
+    }
 }
 
-function transitionObjectInit()
-{
+
+/**
+ * Function for loading dataManager
+ */
+function dataManagerLoadDef() {
+    if (typeof dataManagerLoad != "undefined") {
+        dataManagerLoad();
+    }
+}
+
+
+function showInfow(show, msg) {
+
+    if (show) {
+        $("div.special.info h1").html(msg);
+        //$("div.special.info").css("display","block");
+        containerVisibilitySet("specInfo", true);
+    } else {
+        //$("div.special.info").css("display","none");
+        containerVisibilitySet("specInfo", false);
+    }
+}
+
+
+/**
+ * Gets all elements (divs, which are pages) with attribute data-cont-id and puts them into transitionObject var.
+ */
+function transitionObjectInit() {
     // get all containers
-    $("[data-cont-id]").each(function() {
+    $("[data-cont-id]").each(function () {
         var key = $(this).attr("data-cont-id");
         transitionObject[key] = {
             el: $(this),
             visibility: true
         };
+
+        if(support.supportedTran == 3) {
+            //transitionObject[key].el.one('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd', function()
+            transitionObject[key].el.get(0).addEventListener("webkitTransitionEnd", function()
+            {
+                scriptDef.transition.transitionTranslateEndFunction();
+            }, false);
+        }
     });
     logging("transitionObject found: " + Object.keys(transitionObject).length);
 }
 
-function containerHideAll(expectShowInfo)
-{
+function containerHideAll(expectShowInfo) {
 
-    for(var o in transitionObject){
-        if(o=="specInfo" && expectShowInfo)
-        {
+    for (var o in transitionObject) {
+        if (o == "specInfo" && expectShowInfo) {
         } else
-            containerVisibilitySet(o,false,true);
+            containerVisibilitySet(o, false, true);
     }
 }
 
-function containerVisibilitySet(contID,visibility,forced,ccsStyle)
-{
+function containerVisibilitySet(contID, visibility, forced, ccsStyle) {
 
     // if already visibility is do nothing
-    if(transitionObject[contID].visibility == visibility && forced != true)
-    {
+    if (transitionObject[contID].visibility == visibility && forced != true) {
         return;
     }
 
 
-    if(supportedTran == 3)
-    {
-        if(visibility)
-        {
-            transitionObject[contID].el.css("-webkit-transform","translate3d(0, 0, 0)");
-            transitionObject[contID].el.css("transform","translate3d(0, 0, 0)");
-        } else
-        {
-            transitionObject[contID].el.css("-webkit-transform","translate3d(-"+pageMaxLenght+"px, 0, 0)");
-            transitionObject[contID].el.css("transform","translate3d(-"+pageMaxLenght+"px, 0, 0)");
+    if(support.supportedTran == 3) {
+        if (visibility) {
+            transitionObject[contID].el.css("-webkit-transform", "translate3d(0, 0, 0)");
+            transitionObject[contID].el.css("transform", "translate3d(0, 0, 0)");
+        } else {
+            transitionObject[contID].el.css("-webkit-transform", "translate3d(-" + pageMaxLenght + "px, 0, 0)");
+            transitionObject[contID].el.css("transform", "translate3d(-" + pageMaxLenght + "px, 0, 0)");
         }
-    } else
-    {
-        if(visibility)
-        {
-            if(ccsStyle!=null)
-                transitionObject[contID].el.css("display",ccsStyle);
+    } else {
+        if (visibility) {
+            if (ccsStyle != null)
+                transitionObject[contID].el.css("display", ccsStyle);
             else
-                transitionObject[contID].el.css("display","block");
-        } else
-        {
-            transitionObject[contID].el.css("display","none");
+                transitionObject[contID].el.css("display", "block");
+        } else {
+            transitionObject[contID].el.css("display", "none");
         }
     }
 
@@ -155,24 +195,93 @@ function containerVisibilitySet(contID,visibility,forced,ccsStyle)
 }
 
 
-function transitionInit()
+// contID - new page, old one is not hidden
+//http://coenraets.org/blog/2013/03/hardware-accelerated-page-transitions-for-mobile-web-apps-phonegap-apps/
+function containerSlide(contIDold, contIDnew, direction)
 {
+    if(support.supportedTran!=3)
+    {
+        hideAll();
+        containerVisibilitySet(newPage);
+        return;
+    }
 
-    pageMaxLenght = $(document).width()>$(document).height()?$(document).width():$(document).height();
+    if(contIDold=="")
+    {
+        hideAll();
+        containerVisibilitySet(contIDnew, true);
+        return;
+    }
+
+
+    // prepare, set pages to positions
+    if(direction=="r")
+    {
+        transitionObject[contIDnew].el.css("-webkit-transform", "translate3d(100%, 0, 0)")
+            .css("transform", "translate3d(100%, 0, 0)")
+            .css("transition-duration", "0");
+    } else
+    {
+        transitionObject[contIDnew].el.css("-webkit-transform", "translate3d(-100%, 0, 0)")
+            .css("transform", "translate3d(-100%, 0, 0)");
+    }
+
+
+    scriptDef.transition.transitionInAction = true;
+
+
+    // reflow
+    //console.log(transitionObject[contIDnew].el.get(0).offsetHeight);
+    transitionObject[contIDnew].el.get(0).offsetHeight;
+
+    transitionObject[contIDold].el.css("transition-duration", ".3s");
+    transitionObject[contIDnew].el.css("transition-duration", ".3s");
+
+
+    // start trans
+    if(direction=="r")
+    {
+        transitionObject[contIDold].el.css("-webkit-transform", "translate3d(-100%, 0, 0)")
+            .css("transform", "translate3d(-100%, 0, 0)");
+    } else
+    {
+        transitionObject[contIDold].el.one('webkitTransitionEnd', function(e) {
+            var el = $(e.target);
+            el.css("-webkit-transform", "translate3d(-100%, 0, 0)")
+                .css("transform", "translate3d(-100%, 0, 0)")
+                .css("transition-duration", "0");
+            //$(e.target).remove();
+            /*
+             transitionObject[contIDold].el.css("-webkit-transform", "translate3d(-100%, 0, 0)")
+             .css("transform", "translate3d(-100%, 0, 0)")
+             .css("transition-duration", "0");
+             */
+        });
+        transitionObject[contIDold].el.css("-webkit-transform", "translate3d(100%, 0, 0)")
+            .css("transform", "translate3d(100%, 0, 0)");
+
+    }
+    transitionObject[contIDnew].el.css("-webkit-transform", "translate3d(" + 0 + "px, 0, 0)")
+        .css("transform", "translate3d(" + 0 + "px, 0, 0)");
+
+
+}
+
+
+function transitionInitDef() {
+
+    pageMaxLenght = $(document).width() > $(document).height() ? $(document).width() : $(document).height();
 
     //$("body").css("min-height",pageMaxLenght + "px");
 
     // --- menu bar
-    if(supportedTran == 3)
-    {
-        $("div.menu").css("-webkit-transform","translate3d(-100%, 0, 0)");
-        $("div.menu").css("transform","translate3d(-100%, 0, 0)");
-    } else if(supportedTran == 2)
-    {
-        $("div.menu").css("left","-100%");
-    } else
-    {
-        $("div.menu").css("display","none");
+    if (support.supportedTran == 3) {
+        $("div.menu").css("-webkit-transform", "translate3d(-100%, 0, 0)");
+        $("div.menu").css("transform", "translate3d(-100%, 0, 0)");
+    } else if (support.supportedTran == 2) {
+        $("div.menu").css("left", "-100%");
+    } else {
+        $("div.menu").css("display", "none");
     }
     menuVisible = false;
 
@@ -182,13 +291,14 @@ function transitionInit()
     transitionObjectInit();
     containerHideAll(true);
 
-
+    if (typeof transitionInit != "undefined") {
+        transitionInit();
+    }
 
 }
 
 // improve to hide by 3d transforms current window
-function hideAll()
-{
+function hideAll() {
     /*
      $(".main div.mainContent").css("display","none");
      $(".main div.mainBottom").css("display","none");
@@ -203,22 +313,51 @@ function hideAll()
     containerHideAll();
 }
 
-function supportDetectDef()
-{
-    supportedTran = 1;
+function supportDetectDef() {
 
-    if(supportsTransitions())
-    {
-        supportedTran = 2;
-        logging("support transition",1);
+    // slider hardware support --------------------
+    support.supportedTran = 1;
+
+    if (supportsTransitions()) {
+        support.supportedTran = 2;
+        logging("support transition", 1);
     }
-    if(supportsTransitions3d())
-    {
-        supportedTran = 3;
-        logging("support transition3d",1);
+    if (supportsTransitions3d()) {
+        support.supportedTran = 3;
+        logging("support transition3d", 1);
     }
 
-    if (typeof supportDetect == 'function') { supportDetect(); }
+    if (typeof supportDetect != "undefined") {
+        supportDetect();
+    }
+
+
+    // click event support --------------------
+    if ('ontouchstart' in document.documentElement) {
+        support.supportedTouchStartEven = "touchstart";
+        logging("ontouchstart enabled",1);
+
+    } else
+    {
+        support.supportedTouchStartEven = "click";
+        logging("ontouchstart not supported",1);
+    }
+
+    // TODO its not good
+    // scaner support
+    support.sccanerBarcode = true;
+    if (typeof cordova == "undefined") {
+        support.sccanerBarcode = false;
+    }
+    if(support.sccanerBarcode)
+    {
+
+        if (typeof cordova.plugins.barcodeScanner == "undefined") {
+            support.sccanerBarcode = false;
+        }
+    }
+
+
 }
 
 function supportsTransitions() {
@@ -226,14 +365,18 @@ function supportsTransitions() {
         s = b.style,
         p = 'transition';
 
-    if (typeof s[p] == 'string') { return true; }
+    if (typeof s[p] == 'string') {
+        return true;
+    }
 
     // Tests for vendor specific prop
     var v = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
     p = p.charAt(0).toUpperCase() + p.substr(1);
 
-    for (var i=0; i<v.length; i++) {
-        if (typeof s[v[i] + p] == 'string') { return true; }
+    for (var i = 0; i < v.length; i++) {
+        if (typeof s[v[i] + p] == 'string') {
+            return true;
+        }
     }
 
     return false;
@@ -243,11 +386,11 @@ function supportsTransitions3d() {
     var el = document.createElement('p'),
         has3d,
         transforms = {
-            'webkitTransform':'-webkit-transform',
-            'OTransform':'-o-transform',
-            'msTransform':'-ms-transform',
-            'MozTransform':'-moz-transform',
-            'transform':'transform'
+            'webkitTransform': '-webkit-transform',
+            'OTransform': '-o-transform',
+            'msTransform': '-ms-transform',
+            'MozTransform': '-moz-transform',
+            'transform': 'transform'
         };
 
     // Add it to the body to get the computed style.
